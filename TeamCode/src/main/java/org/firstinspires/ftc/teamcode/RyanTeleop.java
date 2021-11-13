c027517956dd792a72f4ef532e26442a21940fd9
LIT <ryan.leconey28@thalesacademy.org>
2021-11-13 13:00:40 -0500

    RYAN MADE A FUNCTION FOR ROBOT MOVING BACKWARDS, FORWARD, RIGHT, AND LEFT POGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RyanTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RyanTeleop.java
@@ -39,7 +39,7 @@ public class RyanTeleop extends OpMode {
             //If the gamepad is NOT at rest, then we want to see what we need to do.
 
 
-            //This is the code used to run the movement for Tank drivetrain.
+            //This is the code used to run the movement for drivetrain.
             if (Math.abs(gamepad1.right_stick_x) > Math.abs(gamepad1.left_stick_y)) {
 
             if (gamepad1.right_stick_x>0.5) {
