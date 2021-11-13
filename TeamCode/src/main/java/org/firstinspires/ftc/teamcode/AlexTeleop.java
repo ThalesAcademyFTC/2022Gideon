43191834062b78e9439c48b7b992600e0737c8f5
AlexDaCatcher <alexander.larson-dick27@thalesacademy.org>
2021-11-13 12:24:04 -0500

    Coded bumpers and triggers :D

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AlexTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AlexTeleop.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name="AlexTeleop", group="Template")
-@Disabled
+//Disabled
 public class AlexTeleop extends OpMode {
 
     private Anvil robot;
