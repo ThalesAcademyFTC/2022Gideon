a6565cbbea5f8ef4c55966d26b8fa35d736705ab
SpenCzar <spencerpetrikas0@gmail.com>
2021-11-17 16:37:39 -0500

    spenczar is god

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpenCzar_Tele_2022.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="SpenCzar", group="Template")
+@Disabled
+public class SpenCzar_Tele_2022 extends OpMode {
+
+    private Anvil robot;
+
+    @Override
+    public void init() {
+
+        //This code initializes the drivetrain. Make sure that you have the right drivetrain selected!
+        robot = new Anvil(hardwareMap, Anvil.Drivetrain.TANK, telemetry);
+    }
+
+    @Override
+    public void loop() {
+
+        if(gamepad1.left_stick_y>0) {
+            robot.moveForward(gamepad1.left_stick_y);
+        }
+        if(gamepad1.left_stick_y<0) {
+            robot.moveBackward(gamepad1.left_stick_y);
+        }
+        if(gamepad1.left_stick_x>0) {
+            robot.turnRight(gamepad1.left_stick_x);
+        }
+        if(gamepad1.left_stick_x<0) {
+            robot.turnLeft(gamepad1.left_stick_x);
+        }
+        if (gamepad1.x){
+
+        }
+
+        //MOVEMENT
+        //First, we want to make the robot rest if the gamepad is not being touched
+        if (gamepad1.atRest()) robot.rest();
+
+        else {
+
+            //If the gamepad is NOT at rest, then we want to see what we need to do.
+//something
+
+
+            //This is the code used to run the movement for Tank drivetrain.
+            if (Math.abs(gamepad1.left_stick_x) > Math.abs(gamepad1.left_stick_y)) {
+                robot.turnRight(gamepad1.left_stick_x);
+            } else robot.moveBackward(gamepad1.left_stick_y);
+        }
+    }
+}
