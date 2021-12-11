6c8a582b217f28bda271dd4b023db1eb3387e47c
billzR6 <billy.dignam@students.thalesacademy.org>
2021-12-11 13:25:35 -0500

    Lakelan Teleop to move with triggers

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/QuinnAuton2022.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/QuinnAuton2022.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 
-@Autonomous(name="Auton", group="Template")
+@Autonomous(name="QuinnAuton", group="Template")
 //@Disabled
 
 public class QuinnAuton2022 extends LinearOpMode {
