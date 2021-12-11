afd7820c217c07f272f530ddfc728b9e8f9c4f63
Lakelan Jones <lakelan.jones@students.thalesacademy.org>
2021-12-11 10:39:39 -0500

    Lakelan Teleop for Right Stick turning because Spencer's not here to do it.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lakelan_Right_Stick_Turning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lakelan_Right_Stick_Turning.java
@@ -2,18 +2,14 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
+
 
 @TeleOp(name="Lakelan_Right_Stick_Turning", group="Template")
 //@Disabled
-public class Lakelan_Right_Stick_Turing extends OpMode {
+public class Lakelan_Right_Stick_Turning extends OpMode {
 
     private Anvil_With_Back_Wheel_Speed robot;
 
-    public enum SpeedToggle {
-        Normal_Forward,
-        Faster_Forward
-    }
 
     @Override
     public void init() {
@@ -21,12 +17,10 @@ public class Lakelan_Right_Stick_Turing extends OpMode {
         //This code initializes the drivetrain. Make sure that you have the right drivetrain selected!
         robot = new Anvil_With_Back_Wheel_Speed(hardwareMap, Anvil_With_Back_Wheel_Speed.Drivetrain.MECHANUM, telemetry);
     }
-    int speed = 1;
     @Override
-    public void loop(SpeedToggle mode) {
+    public void loop() {
 
-        mode = Lakelan_Right_Stick_Turing.SpeedToggle.Normal_Forward;
-        DcMotor motor1, motor2, motor3, motor4;
+
 
         //MOVEMENT
         //First, we want to make the robot rest if the gamepad is not being touched
@@ -39,8 +33,6 @@ public class Lakelan_Right_Stick_Turing extends OpMode {
                 } else if (gamepad1.left_stick_x < -0.2) {
                     robot.turnLeft(-gamepad1.left_stick_x);
                 }
-
-        }
 
     }
 }
