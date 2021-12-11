e9549055df7edf9618e4caf01c47bd53f2d57590
LIT <ryan.leconey28@thalesacademy.org>
2021-12-11 13:02:05 -0500

    auton test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RyannAuton.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.teamcode.Anvil.Drivetrain.MECHANUM;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@Autonomous(name="Auton", group="Template")
+@Disabled
+
+public class RyannAuton extends LinearOpMode {
+    private Anvil robot;
+    private ElapsedTime runtime = new ElapsedTime();
+    @Override
+    public void runOpMode() {
+        robot = new Anvil(hardwareMap, MECHANUM, telemetry);
+        telemetry.addData("Status", "Initialized");
+        runtime.reset();
+        telemetry.update();
+        //Code Above the waitForStart() is where you define variables or initialize any Vuforia
+        //DO NOT PUT MOVEMENT CODE HERE - YOU WILL BE PENALIZED
+
+        waitForStart(); //Below this point is where you place the linear code for your autonomous.
+        //Any code that goes in this space is only run once, and after it is finished the program ends.
+robot.moveForwardFT(1000,0.5);
+robot.moveRightFT(1000);
+robot.moveBackwardFT(1000,0.5);
+robot.moveLeftFT(1000);
+
+
+        //Inside of the while statement below is any code that you want to run in loop during autonomous.
+        while (opModeIsActive() && runtime.milliseconds() < 30000) {
+
+
+        }
+
+
+    }
+}
