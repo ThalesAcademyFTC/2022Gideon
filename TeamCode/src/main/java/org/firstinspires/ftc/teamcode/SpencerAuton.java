01ce0bf3c8f31b8fc6cc668b3fda849690fa7213
SpenCzar <spencerpetrikas0@gmail.com>
2022-01-15 15:36:35 -0500

    ReReReFixed Skeleton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpencerAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpencerAuton.java
@@ -21,7 +21,7 @@ public class SpencerAuton extends LinearOpMode {
         telemetry.update();
 
         int tcm = 20;
-        int turn = 655;
+        int turn = 652;
         //turn*1 equals 45 degrees, turn*2 equals 90 degrees
 
         //Code Above the waitForStart() is where you define variables or initialize any Vuforia
@@ -35,13 +35,13 @@ public class SpencerAuton extends LinearOpMode {
         robot.moveLeftFT( tcm*50);
         robot.moveForwardFT(tcm*67, 0.5);
         robot.turnRightFT(turn*2, 0.5);
-        robot.moveForwardFT(tcm*65, 1);
+        robot.moveForwardFT(tcm*60, 1);
         sleep( 1000);
         //Throw the rubber ducky onto the platform thingy(carousel)
 
-        robot.moveBackwardFT(tcm*72, 0.5);
+        robot.moveBackwardFT(tcm*75, 0.5);
         sleep( 1000);
-        robot.moveRightFT(tcm*76);
+        robot.moveRightFT(tcm*79);
         sleep( 1000);
         robot.moveForwardFT(tcm*300, 1);
         //end code
