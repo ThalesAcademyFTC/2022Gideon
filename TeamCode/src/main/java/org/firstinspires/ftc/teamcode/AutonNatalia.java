9a9fdd2ac1c3ebd146e9dac2eb78f6f998bcb93a
nataliabern <natalia.bernardo@students.thalesacademy.org>
2022-02-04 17:19:29 -0500

    Added Natalia's Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonNatalia.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonNatalia.java
@@ -26,26 +26,35 @@ public class AutonNatalia extends LinearOpMode {
 
         waitForStart(); //Below this point is where you place the linear code for your autonomous.
         //Any code that goes in this space is only run once, and after it is finished the program ends.
-
-
-        class loop{};; {
             robot.moveForwardFT(15, 0.5);
             //scan for duck sensor?
-            robot.moveLeftFT(1000); //while scanning for duck, until duck is found
-            robot.moveForwardFT(1400, 0.5);
+            robot.clawMotor(0.5);
+            sleep(500);
+            robot.moveLeftFT(1500); //while scanning for duck, until duck is found
+            sleep(500);
+            robot.moveForwardFT(1600, 0.5);
+            sleep(500);
             robot.moveRightFT (1400);
+            sleep(500);
             robot.turnRightFT (1300, 0.5);
+            sleep(500);
+            robot.clawMotor(-0.5);
+            sleep(500);
             robot.turnLeftFT (1200, 0.5);
+            sleep(500);
             robot.moveLeftFT (1400);
-            robot.moveBackwardFT(1450, 0.5);
-            robot.carouselMotor.setPower(1);
-            robot.moveForwardFT(200,0.5);
+            sleep(500);
+            robot.moveBackwardFT(1550,0.5);
+            sleep(500);
+            robot.carouselMotor.setPower(0.5);
+            sleep(500);
+            robot.moveForwardFT(300,0.5);
             //robot.wait();
 
             //robot then has to determine which level to place block, and place it
             //depending on the duck place, that's the level of which to place the block it starts with
             //wait however long maybe til there 5 seconds left and then zoom to the corner hopefully it crosses the barrier
-            /* approxamate measures, used for my guessing bc each mat is 2'x2'
+             /* approxamate measures, used for my guessing bc each mat is 2'x2'
             20tick=1cm
             2000 ticks=about 90 degrees maybe more */
 
@@ -62,7 +71,5 @@ public class AutonNatalia extends LinearOpMode {
 
         Inside of the while statement below is any code that you want to run in loop during autonomous.
         while (opModeIsActive() && runtime.milliseconds() < 30000)*/
-
-        }
     }
 }
